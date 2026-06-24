4c1aee623343fcfe18af8c2a213235c536bb5052
mayang.barde <mayangbarde@gmail.com>
2026-06-24 20:46:06 +0530

    fdgnh

---
+++ python project 2.py
@@ -0,0 +1,20 @@
+# decision making
+# a) elif
+
+
+
+a=12
+b=8
+c=5
+d=23
+
+
+
+if a==b:
+        print("Hello yash")
+elif c<=d:
+        print("hello world")
+elif d>=a:
+        print("hello world2")
+else:
+    print("hello world1")
